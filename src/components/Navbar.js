eda63d51f850b64f28840dd9cbccb9dbc0483bfd
Ameerdhe7 <dhiaaback@gmail.com>
2022-10-08 23:46:36 +0300

    improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import burgerMenu from "../assets/images/burger-menu.png";
 
 const Navbar = () => {
     return (
-        <header className={"bg-accent text-white"}>
-            <div className={"p-4 flex gap-1"}>
+        <header className={"p-4 flex  justify-between bg-accent text-white"}>
+            <div className={" flex gap-1"}>
                 <h1 className={"capitalize"}>HARBOUR.SPACE</h1>
-                <span className={"text-xs mt-0.5"}>/INTERACTION DESIGN</span>
+                <span className={"text-xs mt-0.5 mx-1"}>/INTERACTION DESIGN</span>
+            </div>
+            <div className={"relative"}>
+                <button className={"bg-green-400 py-5 px-6 right-10 absolute  capitalize rounded-full hidden sm:block"}>
+                    apply now
+                </button>
+
+                <img className={"w-7 h-7 sm:hidden"} src={burgerMenu} alt=""/>
             </div>
         </header>
     );
